14b1cbc707143772150d82a50659a881622712ca
4dim4 <hrideshh@yahoo.co.in>
2016-07-13 16:09:23 +0530

    First commit

---
+++ src/test/java/com/niit/shoppingcart/Test.java
@@ -0,0 +1,16 @@
+package com.niit.shoppingcart;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.niit.shoppingcart");
+		context.refresh();
+		context.getBean("category");
+		System.out.println("Category created");
+		context.close();
+	}
+
+}
